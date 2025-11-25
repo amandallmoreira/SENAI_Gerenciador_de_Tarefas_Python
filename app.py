e826834893537a120577d192f53813e2a76f07af
amandallmoreira <amandallmoreira@gmail.com>
2025-11-25 17:12:45 -0300

    Atualizacao25/11
    Versão atualizada com funções e gerador de txt

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from funcoes import *
 continuar_programa = True
 
 # fluxo do programa
-while continuar_programa:    
+while continuar_programa:
+    print(numero_tarefa)
+
+    # salvar número da tarefa para controle
+    salvar_em_arquivo_numero_tarefa()    
     
     # função para imprimir a logo e o menu    
     imprimir_menu()
 
     # captura a opcao do usuario
-    opcao = input("Digite sua opção: ")
+    opcao = input("Escolha uma opção: ")
 
     # cadastrar tarefa
     if opcao == "1":        
@@ -19,14 +23,14 @@ while continuar_programa:
         limpar_tela()
         
         # captura os dados da tarefa com input e os armazena em variáveis
-        print("\n*** DADOS DA TAREFA ***")
+        print("\n=== Nova Tarefa ===")
         
         # captura o título da tarefa com input e o armazena na variável 'titulo'
-        titulo = input("Digite o titulo: ").title()
+        titulo = input("Informe o titulo: ").title()
 
         # controle para impedir título vazio: interrompe o fluxo e retorna ao menu
         if titulo == "":
-            print("\n*** Título obrigatório - Tarefa não cadastrada! ***")            
+            print("\nTítulo é obrigatório. A tarefa não foi cadastrada.")            
             input("\nPressione Enter para continuar...")
             limpar_tela()
             continue
@@ -37,7 +41,7 @@ while continuar_programa:
         # variável controle sair do loop do 'item'
         maisItens = True
         
-        print("*** Digite o item ou 0 para sair. ***\n")
+        print("\nInforme o item (0 para sair)")
 
         # loop que alimenta a lista de itens
         while maisItens:
@@ -52,7 +56,7 @@ while continuar_programa:
         
         # verifica se itens está vazio e interrompe o fluxo caso esteja        
         if itens == []:
-            print("\n*** Item obrigatório - Tarefa não cadastrada! ***")            
+            print("\nItem é obrigatório. A tarefa não foi cadastrada.")            
             input("\nPressione Enter para continuar...")
             limpar_tela()
             continue
@@ -68,30 +72,52 @@ while continuar_programa:
     # marcar tarefa como concluída
     elif opcao == "3":
         limpar_tela()
-        for tarefa in tarefas:                    
-                print(f"\n*** TAREFA {tarefa["numero"]} ***")
-                print(tarefa["Título"])
-                for elemento in tarefa["Item"]:
-                    print(f'- {elemento}')                
-        numero_tarefa = int(input("\nDigite a tarefa que quer marcar como concluída: "))
-        marcar_concluida(numero_tarefa)
-        input("\nPressione Enter para continuar...")        
+        # controle das tarefas que não foram concluídas
+        tarefas_a_deletar = []
+        for tarefa in tarefas:
+                if tarefa["concluida"] == False:
+                    # add na lista das tarefas que não foram concluídas
+                    tarefas_a_deletar.append(str(tarefa["numero"]))        
+                    # exibe as tarefas que não foram concluídas            
+                    exibir_tarefa(tarefa)
+        if tarefas_a_deletar == []:
+            imprimir_art()
+            print("\nNenhuma tarefa pendente.")
+            input("\nPressione 'Enter' para continuar...")
+        else:                
+            numero_tarefa = input("\nNúmero da tarefa: ")            
+            # verifica se usuário escolheu tarefa constante da lista
+            if numero_tarefa in tarefas_a_deletar:
+                marcar_concluida(numero_tarefa)
+            else:
+                # informa erro se opcao não estiver na lista
+                exibir_opcao_invalida()
+                input("\nPressione 'Enter' para continuar...")           
+                    
+        
 
     # deletar tarefa
     elif opcao == "4":
         limpar_tela()
-        for tarefa in tarefas:                    
-                print(f"\n*** TAREFA {tarefa["numero"]} ***")
-                print(tarefa["Título"])
-                for elemento in tarefa["Item"]:
-                    print(f'- {elemento}')
-                if tarefa["concluida"] == True:    
-                    print("Concluída: ✔")        
-                else:
-                    print("Concluída: ❌")
-        numero_tarefa = int(input("\nDigite a tarefa que quer deletar: "))
-        deletar_tarefa(numero_tarefa)
-        input("\nPressione Enter para continuar...")        
+        tarefas_a_deletar = []
+        for tarefa in tarefas:                
+            # add na lista das tarefas que não foram concluídas
+            tarefas_a_deletar.append(str(tarefa["numero"]))        
+            # exibe as tarefas que não foram concluídas            
+            exibir_tarefa(tarefa)
+        if tarefas_a_deletar == []:
+            imprimir_art()
+            print("\nNenhuma tarefa encontrada.")
+            input("\nPressione 'Enter' para continuar...")
+        else:                
+            numero_tarefa = input("\nNúmero da tarefa: ")            
+            # verifica se usuário escolheu tarefa constante da lista
+            if numero_tarefa in tarefas_a_deletar:
+                deletar_tarefa(int(numero_tarefa))
+            else:
+                # informa erro se opcao não estiver na lista
+                exibir_opcao_invalida()
+                input("\nPressione 'Enter' para continuar...")                
 
     # sair do programa
     elif opcao == "0":
@@ -99,7 +125,7 @@ while continuar_programa:
 
     # nenhuma das opções anteriores
     else:
-        print("Opção inválida")
+        exibir_opcao_invalida()
 
     #limpa a tela no Windows
     limpar_tela()
--- funcoes.py
+++ funcoes.py
@@ -7,7 +7,16 @@ tarefas = []
 
 # id para número das tarefas
 #global numero_tarefa
-numero_tarefa = 1
+with open("numero_tarefa.txt", "r") as arq:
+    numero_tarefa = int(arq.read().strip())
+    
+
+def salvar_em_arquivo_numero_tarefa():
+    global numero_tarefa    
+    arquivo = "numero_tarefa.txt"
+    with open(arquivo, "w", encoding="utf-8") as arq:        
+        linha = f"{numero_tarefa}\n"
+        arq.write(linha)
 
 
 # limpa a tela
@@ -21,22 +30,28 @@ def exibir_concluidas():
     for tarefa in tarefas:
         if tarefa["concluida"] == True:
             contador_concluidas += 1
-    print(" " * 40, "Bem-vindo!!")
-    print(" " * 40, f"Tarefas concluídas: {contador_concluidas}/{len(tarefas)}") 
+    print(" " * 40, "Pronto pra organizar seu dia?")
+    print(" " * 40, f"Status: {contador_concluidas}/{len(tarefas)} concluídas") 
 
 # imprime menu
-def imprimir_menu():   
-    """Função que imprime a logo, uma linha abaixo da logo e apresenta o menu com as opções para o usuário."""
+def imprimir_art():
     print(art.logo)
     print("Por Amanda, Fabiana e Marcone")
     print("-" * 50)
+
+def imprimir_menu():   
+    """Função que imprime a logo, uma linha abaixo da logo e apresenta o menu com as opções para o usuário."""
+    imprimir_art()
     exibir_concluidas()
-    print("*** Gerenciador de Tarefas ***")
-    print("1. Cadastrar tarefa")
-    print("2. Listar tarefas")
-    print("3. Marcar tarefa como concluída")
-    print("4. Remover tarefa")
+    print("=== Suas Tarefas ===")
+    print("1. Adicionar")
+    print("2. Listar")
+    print("3. Concluir")
+    print("4. Excluir")
     print("0. Sair")
+
+def exibir_opcao_invalida():
+    print("=== Opção Inválida! ===")
 
 def cadastrar_tarefa(titulo, itens):
     global numero_tarefa
@@ -56,77 +71,100 @@ def cadastrar_tarefa(titulo, itens):
 
     # incremento para id de cada tarefa
     numero_tarefa += 1
+    salvar_em_arquivo_numero_tarefa()
     
     # alimenta a lista de tarefas com o dicionário gerado no passo anterior
     tarefas.append(tarefa)
-    print("\n*** Tarefa cadastrada com sucesso! ***")
+    print("\nTarefa adicionada com sucesso.")
+
+    # salvar tarefa em arquivo
+    salvar_em_arquivo(tarefa)
+    
+def salvar_em_arquivo(tarefa):    
+    arquivo = "tarefas.txt"
+    with open(arquivo, "a", encoding="utf-8") as arq:        
+        numero = tarefa["numero"]
+        titulo = tarefa["Título"]
+        item = tarefa["Item"]
+        concluida = tarefa["concluida"]
+        linha = f"{numero};{titulo};{item};{concluida}\n"
+        arq.write(linha)
 
 def imprimir_menu_listar_tarefas():
-    print("*** Tarefas Cadastradas ***")
-    print("1. Listar todas")
-    print("2. Listar concluídas")
-    print("3. Listar não concluídas")    
+    print("=== Tarefas ===")
+    print("1. Todas")
+    print("2. Concluídas")
+    print("3. Pendentes")    
     print("0. Voltar")
 
 def listar_tarefas():     
     if tarefas == []:
-        print("\n*** Não há tarefas cadastradas! ***")
-        input("\nPressione Enter para continuar...")        
+        print("\nNenhuma tarefa encontrada.")
+        input("\nPressione 'Enter' para continuar...")        
     else:
         limpar_tela()
         imprimir_menu_listar_tarefas()
-        op = input("Digite sua opção: ")
+        op = input("Escolha uma opção: ")
         # listar todas as tarefas
         if op == "1":
             limpar_tela()
             for tarefa in tarefas:                    
-                print(f"\n*** TAREFA {tarefa["numero"]} ***")
-                print(tarefa["Título"])
-                for elemento in tarefa["Item"]:
-                    print(f'- {elemento}')
-                if tarefa["concluida"] == True:    
-                    print("Concluída: ✔")        
-                else:
-                    print("Concluída: ❌")
-            input("\nPressione Enter para continuar...")        
+                exibir_tarefa(tarefa)                
+            input("\nPressione 'Enter' para continuar...")        
+        
         # listar apenas as concluídas                
         elif op == "2":
             limpar_tela()
             concluidas = 0
+            print("=== Tarefas Concluídas ===\n")
             for tarefa in tarefas:                    
                 if tarefa["concluida"] == True:
                     concluidas += 1
-                    print(f"\n*** TAREFA {tarefa["numero"]}:  ✔ ***")
-                    print(tarefa["Título"])
-                    for elemento in tarefa["Item"]:
-                        print(f'- {elemento}')
+                    exibir_tarefa(tarefa)                    
             if concluidas == 0:
-                print("\n*** Não há tarefa concluída! ***")       
-            input("\nPressione Enter para continuar...")                 
+                print("\nNenhuma tarefa concluída.")       
+            input("\nPressione 'Enter' para continuar...")                 
+        
         # listar apenas as não concluídas                
         elif op == "3":
             limpar_tela()
             nao_concluida = 0
+            print("=== Tarefas Pendentes ===\n")
             for tarefa in tarefas:                    
                 if tarefa["concluida"] == False:
                     nao_concluida += 1
-                    print(f"\n*** TAREFA {tarefa["numero"]}:  ❌ ***")
-                    print(tarefa["Título"])
-                    for elemento in tarefa["Item"]:
-                        print(f'- {elemento}')
+                    exibir_tarefa(tarefa)                   
             if nao_concluida == 0:
-                print("\n*** Não há tarefa a ser concluída! ***") 
-            input("\nPressione Enter para continuar...")        
+                print("\nNenhuma tarefa pendente.") 
+            input("\nPressione 'Enter' para continuar...")        
     
         # retornar sem listar
         else:
             return            
         
+
+def exibir_tarefa(tarefa):
+    print(f"\n=== Tarefa {tarefa["numero"]} ===")
+    print(f"Título: {tarefa["Título"]}")
+    print("Itens:")
+    for item in tarefa["Item"]:
+        print(f'- {item}')
+    if tarefa["concluida"] == True:    
+        print("Status: Concluída")        
+    else:
+        print("Status: Pendente")        
+
 def marcar_concluida(numero_tarefa):    
     for tarefa in tarefas:                 
         if tarefa["numero"] == numero_tarefa:            
             tarefa["concluida"] = True
-            print("\n*** Marcada como concluída com sucesso! ***")     
+            print("\nConcluída com sucesso!")
+    opcao = input("Excluir a tarefa concluída? ('1' para sim, 'Enter' para não):")
+    if opcao == "1":
+        deletar_tarefa(int(numero_tarefa))
+        input("\nPressione 'Enter' para continuar...")        
+    else:
+        return    
 
 def deletar_tarefa(numero_tarefa):
     deletou = False
@@ -134,9 +172,9 @@ def deletar_tarefa(numero_tarefa):
         if tarefa["numero"] == numero_tarefa:
             deletou = True
             tarefas.remove(tarefa)
-            print("\n*** Tarefa deletada com sucesso! ***")
+            print("\nDeletada com sucesso.")
     if not deletou:
-        print("\n*** Opção inválida! ***")
+        exibir_opcao_invalida()
                             
         
 
